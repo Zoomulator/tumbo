47833a99f409a9807a8241342fa740e9b2684be2
Kim Simmons <zoomulator@gmail.com>
2014-06-09 17:11:09 +0200

    Sorted predefined types into namespaces.
    Makes it easier to add a specific set of types to your own namespace.

--- types.hpp
+++ types.hpp
@@ -5,40 +5,66 @@
 
 namespace tumbo
     {
-    template<typename T> using scalar = matrix<T,1,1>;
 
-    template<typename T,size_t D> using vec = matrix<T,D,1>;
-    template<typename T> using vec2 = vec<T,2>;
-    template<typename T> using vec3 = vec<T,3>;
-    template<typename T> using vec4 = vec<T,4>;
+    /* All types are in separate namespaces so the user can include a certain
+        set of types into their own namespace. */
+    namespace template_types
+        {
+        template<typename T> using scalar = matrix<T,1,1>;
 
-    template<typename T> using mat22 = matrix<T,2,2>;
-    template<typename T> using mat33 = matrix<T,3,3>;
-    template<typename T> using mat44 = matrix<T,4,4>;
+        template<typename T,size_t D> using vec = matrix<T,D,1>;
+        template<typename T> using vec2 = vec<T,2>;
+        template<typename T> using vec3 = vec<T,3>;
+        template<typename T> using vec4 = vec<T,4>;
 
-    typedef vec2<int> ivec2;
-    typedef vec3<int> ivec3;
-    typedef vec4<int> ivec4;
+        template<typename T> using mat22 = matrix<T,2,2>;
+        template<typename T> using mat33 = matrix<T,3,3>;
+        template<typename T> using mat44 = matrix<T,4,4>;
+        }
 
-    typedef matrix<int, 2, 2> imat22;
-    typedef matrix<int, 3, 3> imat33;
-    typedef matrix<int, 4, 4> imat44;
+    namespace int_types
+        {
+        typedef template_types::vec2<int> ivec2;
+        typedef template_types::vec3<int> ivec3;
+        typedef template_types::vec4<int> ivec4;
 
-    typedef vec2<float> fvec2;
-    typedef vec3<float> fvec3;
-    typedef vec4<float> fvec4;
+        typedef template_types::mat22<int> imat22;
+        typedef template_types::mat33<int> imat33;
+        typedef template_types::mat44<int> imat44;
+        }
 
-    typedef matrix<float, 2, 2> fmat22;
-    typedef matrix<float, 3, 3> fmat33;
-    typedef matrix<float, 4, 4> fmat44;
+    namespace float_types
+        {
+        typedef template_types::vec2<float> fvec2;
+        typedef template_types::vec3<float> fvec3;
+        typedef template_types::vec4<float> fvec4;
 
-    typedef vec2<double> dvec2;
-    typedef vec3<double> dvec3;
-    typedef vec4<double> dvec4;
+        typedef template_types::mat22<float> fmat22;
+        typedef template_types::mat33<float> fmat33;
+        typedef template_types::mat44<float> fmat44;
+        }
 
-    typedef matrix<double, 2, 2> dmat22;
-    typedef matrix<double, 3, 3> dmat33;
-    typedef matrix<double, 4, 4> dmat44;
+    namespace double_types
+        {
+        typedef template_types::vec2<double> dvec2;
+        typedef template_types::vec3<double> dvec3;
+        typedef template_types::vec4<double> dvec4;
+
+        typedef template_types::mat22<double> dmat22;
+        typedef template_types::mat33<double> dmat33;
+        typedef template_types::mat44<double> dmat44;
+        }
+
+    // Include all types into tumbo namespace.
+    namespace types
+        {
+        using namespace template_types;
+        using namespace int_types;
+        using namespace float_types;
+        using namespace double_types;
+        }
+
+    using namespace types;
     } // namespace tumbo
 
 #endif /* TUMBO_TYPES_HPP */
